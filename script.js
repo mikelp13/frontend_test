635f57365191370f73cf9ed0d8baf1d2471b7996
Mykhailo Kovalchuk <mikekovalchuk13@gmail.com>
2020-11-27 22:35:18 +0200

    added some code to script file

---
+++ script.js
@@ -0,0 +1,12 @@
+const element  = document.getElementById('ul'); // assuming ul exists
+const fragment = document.createDocumentFragment();
+const browsers = ['Firefox', 'Chrome', 'Opera', 
+    'Safari', 'Internet Explorer'];
+
+browsers.forEach(function(browser) {
+    const li = document.createElement('li');
+    li.textContent = browser;
+    fragment.appendChild(li);
+});
+
+element.appendChild(fragment);
