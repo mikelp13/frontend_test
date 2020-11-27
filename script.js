489e707c47fb2b4d25ee051b0e7b4066e2710a8f
Mykhailo Kovalchuk <mikekovalchuk13@gmail.com>
2020-11-27 22:45:15 +0200

    apdaded files

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const element  = document.getElementById('ul'); // assuming ul exists
 const fragment = document.createDocumentFragment();
 const browsers = ['Firefox', 'Chrome', 'Opera', 
-    'Safari', 'Internet Explorer'];
+'Safari', 'Internet Explorer'];
 
 browsers.forEach(function(browser) {
-    const li = document.createElement('li');
-    li.textContent = browser;
-    fragment.appendChild(li);
+  const li = document.createElement('li');
+  li.textContent = browser;
+  fragment.appendChild(li);
 });
 
-element.appendChild(fragment);
+ element.appendChild(fragment);
